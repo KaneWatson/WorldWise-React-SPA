4161f7755048227ebfc798626c431d1ae7fae5ab
KaneWatson <webitgenius@gmail.com>
2024-02-25 10:27:26 +0100

    testing netlify bug...

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,9 +3,10 @@ import AppNav from "./AppNav"
 import Footer from "./Footer"
 import Logo from "./Logo"
 import styles from "./SideBar.module.css"
+
 function Sidebar() {
   return (
-    <div className={styles.sidebar}>
+    <div className={"test"}>
       <Logo />
       <AppNav />
       <Outlet />
