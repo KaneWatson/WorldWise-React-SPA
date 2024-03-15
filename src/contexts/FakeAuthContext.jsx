f0756983f1c219d0de20ac2799033fc583488f40
KaneWatson <webitgenius@gmail.com>
2024-03-15 22:00:37 +0100

    changed demo account password to get rid of the annoying password compromised warning

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -21,7 +21,7 @@ function reducer(state, action) {
 const FAKE_USER = {
   name: "Jack",
   email: "jack@example.com",
-  password: "qwerty",
+  password: "qwertyazerty12345",
   avatar: "https://i.pravatar.cc/100?u=zz"
 }
 
